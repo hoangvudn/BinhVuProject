89a7ec608bd2ab433fb71737b518607c63919d08
phanbinh1 <66265249+phanbinh1@users.noreply.github.com>
2021-03-23 18:34:15 +0700

    Users Booking tours

---
+++ UsersPage_Project/src/layout/DefaultLayout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import history from '../util/history';
+import { Select } from 'antd';
+import Header from '../pages/Header';
+import Footer from '../pages/Footer';
+import './styles.css';
+function DefaultLayout({ component: Component, ...props }) {
+  return (
+    <Route 
+    {...props}
+      render={(routerProps) => (
+        <>
+          
+          <Header {...routerProps}/>
+          <div className="main">
+          <Component {...routerProps} 
+          />
+          </div>
+          <Footer {...routerProps}/>
+        </>
+      )}
+    />
+  );
+}
+export default DefaultLayout;
