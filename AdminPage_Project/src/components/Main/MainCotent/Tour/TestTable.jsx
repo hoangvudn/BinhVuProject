30e226d9009061abd9aaa2ddc90f40e73ef3ea74
hoangvudn <LehongphonG@0311!>
2021-04-06 18:20:16 +0700

    Update Tour1

--- AdminPage_Project/src/components/Main/MainCotent/Tour/TestTable.jsx
+++ AdminPage_Project/src/components/Main/MainCotent/Tour/TestTable.jsx
@@ -1,109 +1,75 @@
-.blockNewTour {
-  position: absolute;
-  top: 200px;
-  left: 37%;
-  width: 650px;
-  height: 350px;
-  //background: grey;
-  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+// import React, { useEffect, Component, useState } from "react"
+// import { Link } from 'react-router-dom'
+// import { Formik } from 'formik';
+// import "./style/tourStyle.scss";
+// import { AiOutlineUserAdd } from 'react-icons/ai';
+// import { GoDiffAdded } from 'react-icons/go';
+// import { useDispatch, useSelector } from "react-redux";
+// import { getToursAction } from "../../../../actions/toursActions";
+// import Tour from './Tour';
+// // import { 
+// //     Container, 
+// //     Paper, Box, 
+// //     Typography, TableContainer,
+// //     Table, TableBody,
+// //     TableHead, TableRow, TableCell } from "@material-ui/core";
+// // import { makeStyles } from "@material-ui/core/styles"
+// import './style/tourStyle.scss'
 
-  &__title {
-      position: relative;
-      top: 15px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: gray;
-      // background: yellow;
-  }
-  &__formNew {
-      position: absolute;
-      display: flex;
-      // flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      // background: rebeccapurple;
+// const TestTable = () => { 
+//     const dispatch = useDispatch();
+//     const [users, setUsers] = useState()
+//     useEffect(() => {
+//         const getToursList = () => dispatch(getToursAction());
+//         getToursList(); 
+//     }, []);
     
-      &__inputLeftItem {
-          position: relative;
-          margin-top: 20px;
-          left: -22%;
-      }
-      &__inputImageUrl {
-          position: relative;
-          left: 19px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      &__inputPlace {
-          position: relative;
-          left: 50px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      &__inputTourName {
-          position: relative;
-          left: 44px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      &__inputDay {
-          position: relative;
-          left: 60px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      //----------------------------Right Item--------------------------------------
-      &__inputRightItem {
-          position: relative;
-          margin-top: 20px;
-          left: 10%;
-      }
-      &__inputTransports {
-          position: relative;
-          left: 20px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      &__inputPrice {
-          position: relative;
-          left: 59px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      &__inputStart {
-          position: relative;
-          left: 62px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-      &__inputApply {
-          position: relative;
-          left: 53px;
-          border-top: none;
-          border-left: none;
-          border-right: none;
-      }
-  }
-  
-  &__buttonSave {
-      position: absolute;
-      top: 210px;
-      width: 100px;
-      height: 50px;
-  }
-  &__buttonSave:hover {
-      cursor: pointer; 
-      background: rgb(9, 206, 75);
-      border-radius: 5px;
-      box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
-  }
-}
+//     const toursTravel = useSelector(state => state.toursList.toursList);
+    
+//     console.log("List tour:",toursTravel);
+//     return (
+//         <>
+//           {/* <Container className={classes.root}>
+//               <TableContainer>
+//                   <Table>
+//                       <TableHead>
+//                           <TableRow>
+//                               <TableCell> Name </TableCell>
+//                           </TableRow>
+//                       </TableHead>
+//                   </Table>
+//               </TableContainer>
+//           </Container> */}
+//           {/* <div className="blockTourListMain">
+//                 <table className="blockTourListMain__table">
+//                     <thead>
+//                         <tr className="blockTourListMain__fixed">
+//                             <th className="blockTourListMain__imageUrl">Image URL</th>
+//                             <th className="blockTourListMain__place">Place</th>
+//                             <th className="blockTourListMain__tourName">Tour Name</th>
+//                             <th className="blockTourListMain__dayAmount">Day Amount</th>
+//                             <th className="blockTourListMain__transports">Transport</th>
+//                             <th className="blockTourListMain__price">Price</th>
+//                             <th className="blockTourListMain__startDay">Start Day</th>
+//                             <th className="blockTourListMain__applyPassenger">Apply</th>
+//                         </tr>  
+//                     </thead>
+                    
+//                     <tbody className="blockTourListMain__listItemGroup">  
+//                             {toursTravel.map(tour => (
+//                                 <Tour key={tour.id} tour={tour} />
+//                             ))}
+//                     </tbody>
+                
+                   
+//                 </table>
+               
+//           </div>
+//           <Link to={"/toursList/new" } >  
+//                 <GoDiffAdded className="blockTourListMain__linkToAddTour"/> 
+//           </Link>
+//             <SearchTour /> */}
+//      </>
+//    );
+// }
+// export default  TestTable;
